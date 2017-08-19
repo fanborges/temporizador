b0318ec7a2a4a9490c8f65af30e8d6d28973e480
fanborges <fanborges@gmail.com>
2017-08-19 03:18:03 -0500

    Se ha modificado el tamaño del formulario así como se ha cambiado el título a Temporizador de actividades

--- Reloj/Form1.Designer.cs
+++ Reloj/Form1.Designer.cs
@@ -330,7 +330,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 388);
+            this.ClientSize = new System.Drawing.Size(331, 366);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_total);
@@ -348,7 +348,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Supervisor de actividades";
+            this.Text = "Temporizador de actividades";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
